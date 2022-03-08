fd7b3e3a7b54a81c75c36b7bd10c96dd1a191be3
David Barrow <davecheez21@gmail.com>
2022-03-08 17:02:26 +0000

    Packages and Service Methods interface class

---
+++ src/main/java/com/fusionarc/demo/SharksApplication.java
@@ -0,0 +1,13 @@
+package com.fusionarc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharksApplication.class, args);
+	}
+
+}
